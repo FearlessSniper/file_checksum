ec4fd0a31387c9bed305add7dc66c09290948ace
FearlessSniper <47030588+FearlessSniper@users.noreply.github.com>
2019-11-23 19:44:11 +0800

    Initial commmit. Computes four hashes.
    Implements Tk to open the window to select the file, and bulit-in
    hash library to compute and print hash MD5, SHA1, SHA256, SHA512.

---
+++ hash_checksum.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Compute hash of a file and dumps it into the console.
+"""
+import tkinter as tk
+from tkinter import filedialog
+import hashlib
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    file_path = filedialog.askopenfilename(
+        title="Select a file check its hash")
+    root.destroy()
+    print("File:", file_path, "\nHash:")
+
+    with open(file_path, "rb") as f:
+        bytes_ = f.read()
+	print("MD5: " + hashlib.md5(bytes_).hexdigest())
+	print("SHA1: " + hashlib.sha1(bytes_).hexdigest())
+	print("SHA256: " + hashlib.sha256(bytes_).hexdigest())
+	print("SHA512: " + hashlib.sha512(bytes_).hexdigest())
+        input("Press enter to continue...")
